eb536dbe271b05ee1c0c8385df893053769ba13b
nnthach <nguyenngocthach2301@gmail.com>
2025-08-18 21:47:08 +0700

    Create project

---
+++ app/_layout.jsx
@@ -0,0 +1,17 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+export default function RootLayout() {
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
+    </Stack>
+  );
+}
